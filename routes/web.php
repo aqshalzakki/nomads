8d0a33d372841a2aa22b5759e7b6be49a15720e5
aqshalzakki <kecoatempur2@gmail.com>
2019-12-27 11:43:01 +0700

    route for travel-package and gallery is changed to resource (instead of defining for each route)

--- routes/web.php
+++ routes/web.php
@@ -56,33 +56,8 @@ Route::get('/logout', function(){
             
             Route::get('/', 'DashboardController@index')->name('index');
             
-            Route::prefix('travel-package')
-                ->name('travel-package.')
-                ->group(function(){
-                    Route::get('/', 'TravelPackageController@index')->name('index');
-                    
-                    Route::get('create', 'TravelPackageController@create')->name('create');
-                    Route::post('/', 'TravelPackageController@store')->name('store');
-                    
-                    Route::get('edit/{travelPackage}', 'TravelPackageController@edit')->name('edit');
-                    Route::patch('{travelPackage}', 'TravelPackageController@update')->name('update');
-
-                    Route::delete('/{travelPackage}', 'TravelPackageController@destroy')->name('destroy');
-                });
-
-            Route::prefix('gallery')
-                ->name('gallery.')
-                ->group(function(){
-                    Route::get('/', 'GalleryController@index')->name('index');
-                    
-                    Route::get('create', 'GalleryController@create')->name('create');
-                    Route::post('/', 'GalleryController@store')->name('store');
-                    
-                    Route::get('edit/{gallery}', 'GalleryController@edit')->name('edit');
-                    Route::patch('{gallery}', 'GalleryController@update')->name('update');
-
-                    Route::delete('/{id}', 'GalleryController@destroy')->name('destroy');
-                });            
+            Route::resource('travel-package', 'TravelPackageController');
+            Route::resource('gallery', 'GalleryController');                        
         });
 
 // End Admin routes 
