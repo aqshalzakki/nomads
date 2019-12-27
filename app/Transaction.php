ff7a2205ae19c1cf7e3f05fcc0457829c74a6bef
aqshalzakki <kecoatempur2@gmail.com>
2019-12-27 11:06:03 +0700

    adding some comments in transaction model

--- app/Transaction.php
+++ app/Transaction.php
@@ -9,31 +9,55 @@ class Transaction extends Model
 {
     use SoftDeletes;
 
-    // for testing 
-    protected $guarded = [
-    	'id'
-    ];
-
-    // for real
+    // for testing only
     // protected $guarded = [
-    // 	'id', 'travel_package_id', 'user_id', 'created_at', 'updated_at', 'deleted_at'
+    // 	'id'
     // ];
 
+    // for production
+    protected $guarded = [
+    	'id', 'travel_package_id', 'user_id', 'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    /**
+     * define a relationship with transaction status model
+     *
+     * @return instance of transaction within the status
+     * @author aqshalzakki
+     **/
     public function status()
     {
         return $this->belongsTo(TransactionStatus::class, 'transaction_status_id');
-    }    
+    } 
 
+    /**
+     * define a relationship with transaction detail model
+     *
+     * @return instance of transaction within all of details
+     * @author aqshalzakki
+     **/
     public function details()
     {
     	return $this->hasMany(TransactionDetail::class);
     }
 
+    /**
+     * define a relationship with travel package model
+     *
+     * @return instance of transaction within the travel package
+     * @author aqshalzakki
+     **/
     public function travel_package()
     {
     	return $this->belongsTo(TravelPackage::class);
     }
 
+    /**
+     * define a relationship with user model
+     *
+     * @return instance of transaction within the user
+     * @author aqshalzakki
+     **/
     public function user()
     {
     	return $this->belongsTo(User::class);
