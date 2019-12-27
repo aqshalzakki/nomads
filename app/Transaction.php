64c27208d191d3a3b605614bc493993b115161b4
aqshalzakki <kecoatempur2@gmail.com>
2019-12-27 10:52:03 +0700

    define relationships: status, details, travel package, user

--- app/Transaction.php
+++ app/Transaction.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Transaction extends Model
 {
     use SoftDeletes;
+
+    // for testing 
+    protected $guarded = [
+    	'id'
+    ];
+
+    // for real
+    // protected $guarded = [
+    // 	'id', 'travel_package_id', 'user_id', 'created_at', 'updated_at', 'deleted_at'
+    // ];
+
+    public function status()
+    {
+        return $this->belongsTo(TransactionStatus::class, 'transaction_status_id');
+    }    
+
+    public function details()
+    {
+    	return $this->hasMany(TransactionDetail::class);
+    }
+
+    public function travel_package()
+    {
+    	return $this->belongsTo(TravelPackage::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
