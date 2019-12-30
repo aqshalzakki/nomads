a682664db9c640c93be4bdcfb2f3a87ab5cab1c2
aqshalzakki <kecoatempur2@gmail.com>
2019-12-30 09:03:43 +0700

    revision on details travel package view

--- resources/views/user/travel-package/detail.blade.php
+++ resources/views/user/travel-package/detail.blade.php
@@ -22,113 +22,119 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-8 pl-lg-0">
-            <div class="card card-details">
-              <h1>{{ $travelPackage->title }}</h1>
-              <p>
-                {{ $travelPackage->location }}
-              </p>
-              <div class="gallery">
-                @if($travelPackage->galleries->count())
-                  <div class="xzoom-container">
-                    <img
-                      class="xzoom"
-                      id="xzoom-default"
-                      src="{{ imageStoragePath($travelPackage->galleries->first()->image) }}"
-                      xoriginal="{{ imageStoragePath($travelPackage->galleries->first()->image) }}"
-                    />
-                  </div>
-                @endif
-              
-              <div class="xzoom-thumbs">
-                @foreach ($travelPackage->galleries as $gallery)
-                  <a href="{{ imageStoragePath($gallery->image) }}">
-                    <img 
-                      class="xzoom-gallery"
-                      width="128"
-                      src="{{ imageStoragePath($gallery->image) }}"
-                      xpreview="{{ imageStoragePath($gallery->image) }}"
-                    />
-                  </a>
-                @endforeach
-              </div>
-
-                <h2>About Destination</h2>
+            <div class="col-lg-8 pl-lg-0">
+              <div class="card card-details">
+                <h1>{{ $travelPackage->title }}</h1>
                 <p>
-                  {!! nl2br($travelPackage->about) !!}
+                  {{ $travelPackage->location }}
                 </p>
-                <div class="features row pt-3">
-                  <div class="col-md-4">
-                    <img
-                      src="{{ url('frontend/images/ic_event.png')}}"
-                      alt=""
-                      class="features-image"
-                    />
-                    <div class="description">
-                      <h3>Featured Ticket</h3>
-                      <p>{{ $travelPackage->featured_event }}</p>
+                  <div class="gallery">
+                    @if($travelPackage->galleries->count())
+                      <div class="xzoom-container">
+                        <img
+                          class="xzoom"
+                          id="xzoom-default"
+                          src="{{ imageStoragePath($travelPackage->galleries->first()->image) }}"
+                          xoriginal="{{ imageStoragePath($travelPackage->galleries->first()->image) }}"
+                        />
+                        <div class="xzoom-thumbs">
+                          @foreach($travelPackage->galleries as $gallery)  
+                            <a href="{{ imageStoragePath($gallery->image) }}">
+                              <img
+                                class="xzoom-gallery"
+                                width="128"
+                                src="{{ imageStoragePath($gallery->image) }}"
+                                xpreview="{{ imageStoragePath($gallery->image) }}"
+                              />
+                            </a>
+                          @endforeach
+                        </div>
+                      </div>
+                    @endif
+                    <h2>About Destination</h2>
+                    <p>
+                      {{ $travelPackage->about }}
+                    </p>
+                    <div class="features row pt-3">
+                      <div class="col-md-4">
+                        <img
+                          src="{{ url('frontend/images/ic_event.png') }}"
+                          class="features-image"
+                        />
+                        <div class="description">
+                          <h3>Featured Event</h3>
+                          <p>{{ $travelPackage->featured_event }}</p>
+                        </div>
+                      </div>
+                      <div class="col-md-4 border-left">
+                        <img
+                          src="{{ url('frontend/images/ic_bahasa.png') }}"
+                          alt=""
+                          class="features-image"
+                        />
+                        <div class="description">
+                          <h3>Language</h3>
+                          <p>{{ $travelPackage->language }}</p>
+                        </div>
+                      </div>
+                      <div class="col-md-4 border-left">
+                        <img
+                          src="{{ url('frontend/images/ic_foods.png') }}"
+                          alt=""
+                          class="features-image"
+                        />
+                        <div class="description">
+                          <h3>Foods</h3>
+                          <p>{{ $travelPackage->foods }}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div class="col-md-4 border-left">
-                    <img
-                      src="{{ url('frontend/images/ic_bahasa.png')}}"
-                      alt=""
-                      class="features-image"
-                    />
-                    <div class="description">
-                      <h3>Language</h3>
-                      <p>{{ $travelPackage->language }}</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4 border-left">
-                    <img
-                      src="{{ url('frontend/images/ic_foods.png')}}"
-                      alt=""
-                      class="features-image"
-                    />
-                    <div class="description">
-                      <h3>Foods</h3>
-                      <p>{{ $travelPackage->foods }}</p>
-                    </div>
-                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="card card-details card-right">
+                <h2>Members are going</h2>
+                <div class="members my-2">
+                  <img src="{{ url('frontend/images/members.png') }}" alt="" class="w-75" />
                 </div>
+                <hr />
+                <h2>Trip Informations</h2>
+                <table class="trip-informations">
+                  <tr>
+                    <th width="50%">Date of Departure</th>
+                    <td width="50%" class="text-right">{{ Carbon\Carbon::create($travelPackage->departure_date)->format('F n, Y') }}</td>
+                  </tr>
+                  <tr>
+                    <th width="50%">Duration</th>
+                    <td width="50%" class="text-right">{{ $travelPackage->duration }}</td>
+                  </tr>
+                  <tr>
+                    <th width="50%">Type</th>
+                    <td width="50%" class="text-right">{{ $travelPackage->type }}</td>
+                  </tr>
+                  <tr>
+                    <th width="50%">Price</th>
+                    <td width="50%" class="text-right">${{ $travelPackage->price }} / person</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="join-container">
+                  @auth
+                    <form action="#" method="post">
+                      <button type="submit" class="btn btn-block btn-join-now mt-3 py-2">
+                        Join Now
+                      </button>
+                    </form>
+                  @endauth
+                  @guest
+                    <a href="{{ route('login') }}" class="btn btn-block btn-join-now mt-3 py-2">
+                      Login to Join
+                    </a>
+                  @endguest
               </div>
             </div>
           </div>
-          <div class="col-lg-4">
-            <div class="card card-details card-right">
-              <h2>Members are going</h2>
-              <div class="members my-2">
-                <img src="{{ url('frontend/images/members.png')}}" alt="" class="w-75" />
-              </div>
-              <hr />
-              <h2>Trip Informations</h2>
-              <table class="trip-informations">
-                <tr>
-                  <th width="50%">Date of Departure</th>
-                  <td width="50%" class="text-right">22 Aug, 2019</td>
-                </tr>
-                <tr>
-                  <th width="50%">Duration</th>
-                  <td width="50%" class="text-right">4D 3N</td>
-                </tr>
-                <tr>
-                  <th width="50%">Type</th>
-                  <td width="50%" class="text-right">Open Trip</td>
-                </tr>
-                <tr>
-                  <th width="50%">Price</th>
-                  <td width="50%" class="text-right">$80,00 / person</td>
-                </tr>
-              </table>
-            </div>
-            <div class="join-container">
-              <a href="{{ route('checkout.index') }}" class="btn btn-block btn-join-now mt-3 py-2"
-                >Join Now</a
-              >
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   </main>
